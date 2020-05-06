a4a28a0bfe0e11e4298a63222f11409a7e7a0ff1
mo7md98zizo <mo7md98zizo>
2020-05-06 21:39:13 +0200

    reports1

--- app/controllers/ReportController.php
+++ app/controllers/ReportController.php
@@ -11,4 +11,18 @@ class ReportController extends Controller
     public function Sales(){
         $this->viewHome("sales");
     }
+    public function showReciept($branchId,$memberId,$contractId){
+#require "/GYM/app/models/FPDF/fpdf.php";
+        require_once '../app/models/FPDF/fpdf.php';
+
+        $pdf=new FPDF();
+        $header = array('Country', 'Capital', 'Area (sq km)', 'Pop. (thousands)','id','type','amount','sas','id','type','amount','sas','id','type','amount','sas');
+        $data = array(array('egypt','cairo','22','100','1','7lwa','50','55','1','7lwa','50','55','1','7lwa','50','55'));
+        $pdf->SetFont('Arial','',7);
+        $pdf->AddPage();
+        $pdf->FancyTable($header,$data);
+        $pdf->Output();
+
+    }
+
 }
